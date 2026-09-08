ce5b82b6e4d83d76ca01c68af518d6588bd8a09c
lu-ramya <lu.ramya@partner.ibm.com>
2026-09-08 10:05:09 +0000

    pmc vm warning state

--- app/models/manageiq/providers/ibm_cloud/inventory/parser/power_virtual_servers.rb
+++ app/models/manageiq/providers/ibm_cloud/inventory/parser/power_virtual_servers.rb
@@ -54,6 +54,8 @@ class ManageIQ::Providers::IbmCloud::Inventory::Parser::PowerVirtualServers < Ma
         :vendor            => "ibm_power_vs",
         :connection_state  => "connected",
         :raw_power_state   => instance.status,
+        :health_state      => instance.health&.status&.downcase,
+        :health_details    => instance.health&.reason,
         :uid_ems           => instance.pvm_instance_id,
         :format            => instance.storage_type,
         :placement_group   => persister.placement_groups.lazy_find(instance.placement_group),
